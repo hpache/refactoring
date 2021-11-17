33f7abfc7a8c05006cd508c586485372751a45cc
xvrmrkwtz <xmmark23@colby.edu>
2021-11-16 23:31:50 -0500

    added back global code and main function

--- after/connect-4-after.py
+++ after/connect-4-after.py
@@ -101,3 +101,74 @@ def check_diagonals(grid, player):
                 and grid[row+3][col+3] == 1:
                     return True
 
+
+# setting up the window
+window = make_window("Connect 4", "light sky blue", 800, 600)
+
+
+# the grid
+grid = []
+
+for rows in range(5):
+    grid.append([0]*7)
+
+# drawing_turtle
+my_turtle = make_turtle('classic', "white", 1, 1, 0, 0 )
+
+x_offset = -150
+y_offset = 200
+tile_size = 50
+
+player_1_turn = True
+
+def make_move(point, player_1_turn):
+
+    row = int(abs((point['x'] - y_offset - 25) // (50) + 1))
+    col = int(abs((point['y'] - x_offset - 25) // (50) + 1))
+
+    if player_1_turn == True:
+        grid[row][col] = 1
+    else:
+        grid[row][col] = 2
+
+    if player_1_turn == True:
+        player_1_turn = False
+    else:
+        player_1_turn = True
+
+window.onscreenclick(make_move)
+window.listen()
+
+def main():
+
+    global player_1_turn
+    
+    draw_grid(grid, my_turtle, x_offset, y_offset, tile_size)
+    while True:
+
+        selected_row = int(input("enter row, player "+ str(turn) +": "))
+        selected_col = int(input("enter col, player "+ str(turn) +": "))
+
+        if grid[selected_row][selected_col] == 0:
+
+            if player_1_turn == True:
+                grid[selected_row][selected_col] = 1
+            else:
+                grid[selected_row][selected_col] = 2
+
+        draw_grid(grid, my_turtle, -150, 200, 50)
+        window.update()
+
+        if check_winner(grid, 1):
+            print("player 1 won")
+
+        elif check_winner(grid, 2):
+            print("player 2 won")
+
+        if player_1_turn == True:
+            player_1_turn = False
+        else:
+            player_1_turn = True
+
+if __name__ == "__main__":
+	main()
